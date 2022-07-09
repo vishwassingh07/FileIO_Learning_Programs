19297b37a574b391a9f562eaa40ed053524fe560
Vishwas <baghel.vishwas0@gmail.com>
2022-07-09 15:13:07 +0530

    [Vishwas]add: Checking Whether The File Exists Or Not

--- FileIO_Learning_Programs/Program.cs
+++ FileIO_Learning_Programs/Program.cs
@@ -6,6 +6,20 @@ namespace FileIO_Learning_Programs
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To The File IO Learning Programs\n========================================");
+            string filePath = @"D:\RFP BATCH 157\FileIO Learning Prorams\FileIO_Learning_Programs\FileIO_Learning_Programs\FileIO.txt";
+            DoesFileExist(filePath);
+        }
+        
+        public static void DoesFileExist(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine("File Exists ");
+            }
+            else
+            {
+                Console.WriteLine("File Does Not Exist");
+            }
         }
     }
 }
