bb215b36e08fee2b7d89fc08c8f7e5c5f49ef696
Vishwas <baghel.vishwas0@gmail.com>
2022-07-09 16:02:26 +0530

    [Vishwas]add: Reading All Lines At Once

---
+++ FileIO_Learning_Programs/FileIOOperations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO_Learning_Programs
+{
+    public class FileIOoperations
+    {
+        //Checking Whether File Exists Or NOt
+        public static bool DoesFileExist(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine("File Exists ");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("File Does Not Exist");
+                return false;
+            }
+        }
+        //Reading Line By Line
+        public static void ReadLineByLine(string filePath)
+        {
+            if (DoesFileExist(filePath))
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+                //return true
+            }
+        }
+        //Reading All Line At Once
+        public static void ReadAtOnce(string filePath)
+        {
+            if (DoesFileExist(filePath))
+            {
+                string line = File.ReadAllText(filePath);
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
--- FileIO_Learning_Programs/Program.cs
+++ FileIO_Learning_Programs/Program.cs
@@ -7,34 +7,38 @@ namespace FileIO_Learning_Programs
         {
             Console.WriteLine("Welcome To The File IO Learning Programs\n========================================");
             string filePath = @"D:\RFP BATCH 157\FileIO Learning Prorams\FileIO_Learning_Programs\FileIO_Learning_Programs\FileIO.txt";
-            DoesFileExist(filePath);
-            ReadLineByLine(filePath);
+            bool end = true;
+            Console.WriteLine("1. Checking File Exists Or Not\n2. Reading Line By Line\n3. Reading All Lines At Once\n4. " +
+                "End The Program");
+            while (end)
+            {
+                Console.Write("Choose an option to execute : ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FileIOoperations.DoesFileExist(filePath);
+                        break;
+                    case 2:
+                        FileIOoperations.ReadLineByLine(filePath);
+                        break;
+                    case 3:
+                        FileIOoperations.ReadAtOnce(filePath);
+                        break;
+                    case 4:
+                        end = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option ");
+                        break;
+                       
+                }
+            }
         }
         
-        public static bool DoesFileExist(string filePath)
-        {
-            if (File.Exists(filePath))
-            {
-                Console.WriteLine("File Exists ");
-                return true;
-            }
-            else
-            {
-                Console.WriteLine("File Does Not Exist");
-                return false;
-            }
-        }
-        public static void ReadLineByLine(string filePath)
-        {
-            if (DoesFileExist(filePath))
-            {
-                string[] lines = File.ReadAllLines(filePath);
-                foreach (string line in lines)
-                {
-                    Console.WriteLine(line);
-                }
-                //return true
-            }
-        }
+       
+       
+       
+       
     }
 }
