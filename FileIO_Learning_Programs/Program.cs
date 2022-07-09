ece5a4c920fe4bce8fc8fbd5a84571d3836d7f81
Vishwas <baghel.vishwas0@gmail.com>
2022-07-09 15:44:36 +0530

    [Vishwas]add: Reading Lines One By One

--- FileIO_Learning_Programs/Program.cs
+++ FileIO_Learning_Programs/Program.cs
@@ -8,17 +8,32 @@ namespace FileIO_Learning_Programs
             Console.WriteLine("Welcome To The File IO Learning Programs\n========================================");
             string filePath = @"D:\RFP BATCH 157\FileIO Learning Prorams\FileIO_Learning_Programs\FileIO_Learning_Programs\FileIO.txt";
             DoesFileExist(filePath);
+            ReadLineByLine(filePath);
         }
         
-        public static void DoesFileExist(string filePath)
+        public static bool DoesFileExist(string filePath)
         {
             if (File.Exists(filePath))
             {
                 Console.WriteLine("File Exists ");
+                return true;
             }
             else
             {
                 Console.WriteLine("File Does Not Exist");
+                return false;
+            }
+        }
+        public static void ReadLineByLine(string filePath)
+        {
+            if (DoesFileExist(filePath))
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+                //return true
             }
         }
     }
